1cdfe0f6c01667f06a3d776940b80e3dc3a740d6
Alibek <wfwfwfwfwf96@mail.ru>
2020-11-02 19:19:05 +0600

    smt

--- Desktop/cinema_app/src/App.js
+++ Desktop/cinema_app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Cinema from './components/Cinema';
 
 function App() {
   return (
---
+++ Desktop/cinema_app/src/Components/Cinema.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card, CardBody, Button, CardTitle, CardText, CardImg } from 'reactstrap';
+
+const Example = (props) => {
+    return (
+        <div>
+            <Card>
+          <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card Title</CardTitle>
+            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+            <CardText>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </CardText>
+          </CardBody>
+        </Card>
+        </div>
+        export default Example;
