23299e2cae870650538dbe7b76da4ba2a2c7e585
Alibek <wfwfwfwfwf96@mail.ru>
2020-11-03 16:00:41 +0600

    end

--- Desktop/cinema_app/src/App.js
+++ Desktop/cinema_app/src/App.js
@@ -1,25 +1,14 @@
 import React, {Component} from 'react';
-import Cinema from './components/Cinema';
+import Cinema from './Components/Cinema';
 
-function App() {
+function  App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Cinema">
+      <Cinema title="Avatar" text="dangerous something also too fantastick" />
+      <Cinema title="Terminator" text="idea of this film its to show what can return progressing in ii programming robots" />
+      <Cinema title="Incredible pizza" text="Pizza from Naryn city, with meal that can feel you like a SuperMan or even better" />
     </div>
   );
-}
+};
 
 export default App;
